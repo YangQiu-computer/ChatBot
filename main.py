79e9d225fc436ed3369b1807c0ed03f1f89bad86
杨俅 <1310792308@qq.com>
2024-08-21 17:41:16 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from langchain.memory import ConversationBufferMemory
-
 from utils import get_chat_response
 
-st.title("💬 ChatBot")
+st.title("💬 克隆ChatGPT")
 
 with st.sidebar:
     openai_api_key = st.text_input("请输入OpenAI API Key：", type="password")
@@ -11,7 +10,7 @@ with st.sidebar:
 
 if "memory" not in st.session_state:
     st.session_state["memory"] = ConversationBufferMemory(return_messages=True)
-    st.session_state["messages"] = [{"role": "ai","content": "你好，我是你的AI聊天机器人，有什么可以帮你的吗？"}]
+    st.session_state["messages"] = [{"role": "ai","content": "你好，我是你的AI助手，有什么可以帮你的吗？"}]
 
 for message in st.session_state["messages"]:
     st.chat_message(message["role"]).write(message["content"])
@@ -25,8 +24,7 @@ if prompt:
     st.chat_message("human").write(prompt)
 
     with st.spinner("AI正在思考中，请稍等..."):
-        response = get_chat_response(prompt, st.session_state["memory"],
-                                     openai_api_key)
+        response = get_chat_response(prompt, st.session_state["memory"],openai_api_key)
     msg = {"role": "ai", "content": response}
     st.session_state["messages"].append(msg)
     st.chat_message("ai").write(response)
